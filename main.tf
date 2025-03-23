fb2e02fd1579ca411141f96ee75e2e97d661e7cc
Ubuntu <ubuntu@ip-172-31-36-78.eu-west-1.compute.internal>
2025-03-23 20:42:57 +0000

    file added

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource local_file my_file {
+               filename="Automate"
+               content="This is a file for Terraform"
+}
---
+++ s3.tf
@@ -0,0 +1,7 @@
+# this is for S3 bucket
+
+resource "aws_s3_bucket" "my_bucket" {
+              
+            bucket = "var-priya-terraform-bucket"
+
+}
